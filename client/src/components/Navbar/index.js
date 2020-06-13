9e73430a0d68879701306f2af56b6988605c2251
alexisysanabria <alexisyep@gmail.com>
2020-06-13 11:13:52 -0400

    sidebar added to mobile views

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,6 +2,21 @@ import React, { Component } from "react";
 import "./style.scss";
 
 export default class index extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hamburger: false,
+    };
+  }
+
+  toggleHamburger = async () => {
+    await this.setState({
+      hamburger: !this.state.hamburger,
+    });
+    console.log(this.state.hamburger);
+  };
+
   render() {
     return (
       <nav className="nav-bar-container">
@@ -16,8 +31,10 @@ export default class index extends Component {
         </div>
         <ul className="menu">
           <div className="hamburger">
-            <i className="fas fa-bars"></i>
+            <i onClick={this.toggleHamburger} className="fas fa-bars"></i>
           </div>
+
+          {/* Navbar */}
           <li className="nav-link nav-link__home">
             <a className="links-home" href="/">
               Home
@@ -39,6 +56,31 @@ export default class index extends Component {
             </a>
           </li>
         </ul>
+        {/* Sidebar */}
+        {this.state.hamburger ? (
+          <nav className="sidebar">
+            <li className="nav-link-sidebar nav-link__home">
+              <a className="links-home" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-link-sidebar nav-link__about">
+              <a className="links-home" href="/about">
+                About
+              </a>
+            </li>
+            <li className="nav-link-sidebar nav-link__products">
+              <a className="links-home" href="/products">
+                Create my product
+              </a>
+            </li>
+            <li className="nav-link-sidebar nav-link__contact">
+              <a className="links-home" href="/contact">
+                Contact
+              </a>
+            </li>
+          </nav>
+        ) : null}
       </nav>
     );
   }
