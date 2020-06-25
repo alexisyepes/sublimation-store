b52d4df2df2166bd4ff0beb1a79b47dbf9674aac
alexisysanabria <alexisyep@gmail.com>
2020-06-25 14:38:11 -0400

    changed stripe to live mode

--- client/src/components/StripeForm/index.js
+++ client/src/components/StripeForm/index.js
@@ -274,7 +274,7 @@ const Index = (props) => {
   };
 
   const stripePromise = loadStripe(
-    "pk_test_5Oyk1YnGn6dPTwNAJ5KhOIcN00lcwk3Oqk"
+    "pk_live_X5VIPRMcJH7dLUplWTMEgTmg00DLrg3Eu6" //Live mode
   );
 
   return (
